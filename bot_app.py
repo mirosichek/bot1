e8f1ab43d4386e49072a9ae0f2874469d0ccb659
mirosick <mirosickimya@gmail.com>
2025-11-29 13:35:50 +0300

    work

--- bot_app.py
+++ bot_app.py
@@ -1,13 +1,12 @@
-import asyncio
 from aiogram import Bot, Dispatcher
 from registrtion import registration_router
 from inline_key import quiz_ruoter
 
 class BotApp:
-    def _init_(self, token, db):
-        self.bot = Bot(token)
-        self.db = db
+    def __init__(self, token, db):  # <- Этот конструктор должен быть!
+        self.bot = Bot(token=token)
         self.dp = Dispatcher()
+        self.db = db
         self.dp.include_router(registration_router)
         self.dp.include_router(quiz_ruoter)
 
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 
 class Database:
-    def __init__(self, url: str, key: str):
+    def __init__ (self, url: str, key: str):
         self.supabase: Client = create_client(url, key)
 
     def add_user(self, name: str, surname: str):
